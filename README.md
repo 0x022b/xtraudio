79017d104b682ae1d7fa311a2ece7fad517c8e67
Janne K <0x022b@gmail.com>
2014-12-05 23:04:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# [xtraudio][1]
+
+xtraudio is a [Python][2] script that splits any [FFmpeg][3] supported video
+containing an AAC audio track into individual audio tracks and extracts a
+thumbnail image from the video.
+
+## Usage
+
+    ./xtraudio.py /usr/bin/ffmpeg video.mp4 tracks.txt extracted/
+
+## Tracklisting Format
+
+Tracklisting file must contain a single track per line and the lines must be
+formatted by separating track start times from the track title by using a
+vertical bar.
+
+	00:00|Track 1
+	03:15|Track 2
+	06:30|Track 3
+
+## Output Filenames
+
+Output filenames are genereted by using the track's sequence number as a zero
+padded prefix followed by the track title separated by a hyphen. Tracklisting
+used in the previous example would generate the following files.
+
+	01 - Track 1.m4a
+	02 - Track 2.m4a
+	03 - Track 3.m4a
+
+[1]: https://bitbucket.org/scoobadog/xtraudio "xtraudio"
+[2]: https://www.python.org/ "Python.org"
+[3]: https://www.ffmpeg.org/ "FFmpeg"
---
+++ xtraudio.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+import subprocess
+import sys
+from argparse import ArgumentParser
+from os import path
+
+def main():
+	parser = ArgumentParser(description="splits a file into M4A tracks")
+	parser.add_argument("ffmpeg", action="store", type=str, help="path to ffmpeg")
+	parser.add_argument("source", action="store", type=str, help="file to split")
+	parser.add_argument("tracks", action="store", type=str, help="track info file")
+	parser.add_argument("target", action="store", type=str, help="target folder")
+	options = parser.parse_args()
+
+	times, names = [], []
+	with open(options.tracks, "r") as tracks:
+		for track in [t.strip() for t in tracks]:
+			fields = track.split("|")
+			if len(fields) == 2:
+				times.append(fields[0])
+				if len(times) > 2:
+					times = times[1:]
+
+				if len(times) == 2:
+					subprocess.call([
+						options.ffmpeg, "-i", options.source, "-ss", times[0],
+						"-vn", "-codec:a", "copy", "-sn", "-to", times[1],
+						path.join(options.target, "{0:02d} - {1}.m4a".format(len(names), names[-1]))
+					])
+
+				names.append(fields[1])
+
+	subprocess.call([
+		options.ffmpeg, "-i", options.source, "-ss", times[-1],
+		"-vn", "-codec:a", "copy", "-sn",
+		path.join(options.target, "{0:02d} - {1}.m4a".format(len(names), names[-1]))
+	])
+
+	subprocess.call([
+		options.ffmpeg, "-ss", times[-1], "-i", options.source,
+		"-filter:v",  "thumbnail", "-frames:v",  "1",
+		path.join(options.target, "folder.jpg")
+	])
+
+if __name__ == "__main__":
+	try:
+		sys.exit(main())
+	except KeyboardInterrupt:
+		sys.exit(1)
